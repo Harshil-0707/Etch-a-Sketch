b5f5a69647eec1d88a20efd016921bcabfce7536
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-04-17 23:26:30 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const rows = document.querySelector(".gridRow");
+const cols = document.querySelector(".gridCol");
+const container = document.querySelector("#innerContainer");
+
+// TODO : Fix grid row and column bug
+
+// function makRows(totalRows) {
+//   for (let i = 0; i < totalRows; i++) {
+//     let row = document.createElement("div");
+//     container.appendChild(row).className = "gridRow";
+//   }
+// }
+
+// function makeCols(totalCols) {
+//   for (let j = 0; j < totalCols; j++) {
+//     let col = document.createElement("div");
+//     container.appendChild(col).className = "gridCol";
+//   }
+// }
+// makRows(16);
+// makeCols(16);
