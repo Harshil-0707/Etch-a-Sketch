166f6ba6f05066f12ec34a225dc4101ccb7b42a6
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-04-21 21:01:38 +0530

    Changed style for input type range

---
+++ README.md
@@ -0,0 +1,1 @@
+This a Sketch project🖌️
--- main.js
+++ main.js
@@ -1,27 +1,95 @@
+let val = 0;
 const btns = document.querySelectorAll(".btn");
-const rows = document.querySelector(".gridRow");
-const cols = document.querySelector(".gridCol");
 const gridRange = document.querySelector("#gridRange");
 const pickColor = document.querySelector(".pickColor");
-const container = document.querySelector("#innerContainer");
+const clearGridBtn = document.querySelector("#clearGridBtn");
+const gridContainer = document.querySelector("#innerContainer");
 const GridRangeValue = document.querySelector(".GridRangeHeading");
+
+let mouseDown = false;
+gridContainer.onmouseup = () => (mouseDown = false);
+gridContainer.onmousedown = () => (mouseDown = true);
 
 btns[0].classList.add("foucs-cls");
 GridRangeValue.textContent = `${gridRange.value} X ${gridRange.value}`;
+
+// create specified number cells in container when DOM is ready
+(function () {
+  createGrid(gridRange.value);
+  fillGridColor();
+})();
+
+function fillGridColor() {
+  const gridCell = document.querySelectorAll(".gridCell");
+  val = gridCell;
+  gridCell.forEach((gc) => {
+    gc.addEventListener("mouseenter", (e) => {
+      if (mouseDown === true) {
+        e.currentTarget.style.backgroundColor = `${pickColor.value}`;
+      }
+    });
+    gc.addEventListener("click", (e) => {
+      e.currentTarget.style.backgroundColor = `${pickColor.value}`;
+    });
+  });
+}
+
+pickColor.onchange = () => {
+  btns[0].style.backgroundColor = `${pickColor.value}`;
+
+  pickColor.value >= "#BCA4A4"
+    ? (btns[0].style.color = "#000000")
+    : (btns[0].style.color = "#ffffff");
+};
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     btns.forEach((btn) => {
       btn.classList.remove("foucs-cls");
     });
+    e.currentTarget.value === "Eraser" ? erase(val) : fillGridColor();
     e.currentTarget.classList.add("foucs-cls");
   });
 });
 
-gridRange.onchange = (e) => {
-  changeGridSize(e.currentTarget.value);
-};
+gridRange.addEventListener("input", function () {
+  GridRangeValue.textContent = `${this.value} x ${this.value}`;
+  createGrid(this.value);
+});
 
-function changeGridSize(gridSize) {
-  GridRangeValue.textContent = `${gridSize} x ${gridSize}`;
+clearGridBtn.onclick = () => clearGrid(val);
+
+function createGrid(gridSize) {
+  gridContainer.innerHTML = "";
+  drawGrid(gridSize);
+  fillGridColor();
+}
+
+// Remove color from clicked grid cell
+function erase(gridCell) {
+  gridCell.forEach((gc) => {
+    gc.addEventListener("click", (e) => {
+      e.currentTarget.style.backgroundColor = "transparent";
+    });
+  });
+}
+
+//Remove color from every cell of the grid
+function clearGrid(gridCell) {
+  gridCell.forEach((gc) => {
+    gc.style.backgroundColor = "transparent";
+  });
+}
+
+// Draw grid and change it's size
+function drawGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "gridCell");
+      gridContainer.appendChild(newDiv);
+      gridContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+      gridContainer.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+    }
+  }
 }
